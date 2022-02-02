5fac5d6c618666aa0589656a176afee16065e1af
Hanny0809 <chohyeonhee95@gmail.com>
2022-02-02 09:26:52 +0900

    Start Project

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+// import autoprefixer from 'autoprefixer' 
+const autoprefixer = require('autoprefixer')
+
+// export {
+//    Plugins: c[
+//      autoprefixer
+//       ]
+// }
+module.exports = {
+  Plugins: [
+    autoprefixer
+  ]
+}
